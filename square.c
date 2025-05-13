6e2685e5424c16ee7d285f282214a47cf270c942
preetheiga <preetheiga2006@gmail.com>
2025-05-13 15:20:45 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,sq,cube;
+    n=2;
+    sq=n*n;
+    printf("square of the number is %d\n",sq);
+    cube=n*n*n;
+    printf("cube of the number is %d\n",cube);
+    
+    
+}
