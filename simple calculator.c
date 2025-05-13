bc4d8aa60ac513564511dee5f170676434a5374e
preetheiga <preetheiga2006@gmail.com>
2025-05-13 14:18:30 +0530

    Add files via upload

---
+++ simple calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int b;
+    a=10;
+    b=5;
+    printf("addition for a+b is: %d \n",a+b);
+    printf("subraction for a-b is: %d \n",a-b);
+    printf("multiplication for a*b is: %d \n",a*b);
+    printf("division for a/b is: %d \n",a/b);
+    return 0;
+}
