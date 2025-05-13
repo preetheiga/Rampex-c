4d4e650fec78237c79eb2c855ead0e54dafad874
preetheiga <preetheiga2006@gmail.com>
2025-05-13 14:18:56 +0530

    Add files via upload

---
+++ print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int name;
+    int age;
+    int salary;
+    printf("Enter the name:");
+    scanf("%d \n",name);
+    printf("Enter the age:\n");
+    scanf("%d",age);
+    printf("Enter the salary of an employee:\n");
+    scanf("%d \n",salary);
+    return 0;
+}
